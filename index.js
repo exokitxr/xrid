a21a2c78509b51f34ff96a558e9e3e8566c9295c
Avaer Kazmer <a@modules.io>
2018-05-20 05:07:40 -0400

    Authentication handling cleanup

--- index.js
+++ index.js
@@ -552,14 +552,23 @@ const _elementSetter = (self, attribute, cb) => {
   }
 };
 
+class XRIDEvent {
+  constructor(type) {
+    this.type = type;
+  }
+}
+module.exports.XRIDEvent = XRIDEvent;
+
 class XRID extends EventEmitter {
   constructor({url, username, password, token} = {}) {
     super();
 
+    let user = null;
+
     const _throwError = e => {
       Promise.resolve()
         .then(() => {
-          const e = new XRMultiplayerEvent('error');
+          const e = new XRIDEvent('error');
           e.error = error;
           this.emit(e.type, e);
         });
@@ -567,9 +576,53 @@ class XRID extends EventEmitter {
 
     if (url) {
       if (username && password) {
-        // XXX
+        fetch(url + '/l', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            username,
+            password,
+          }),
+        })
+          .then(res => {
+            if (res.statusCode >= 300 && res.statusCode < 300) {
+              return res.json();
+            } else {
+              return Promise.reject(new Error(`got invalid status code ${res.statusCode}`));
+            }
+          })
+          .then(newUser => {
+            user = newUser;
+
+            const e = new XRIDEvent('authenticate');
+            e.user = user;
+            this.emit(e.type, e);
+          })
+          .catch(err => {
+            _throwError(err);
+          });
       } else if (token) {
-        // XXX
+        fetch(url + '/l', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            token,
+          }),
+        })
+          .then(res => {
+            if (res.statusCode >= 300 && res.statusCode < 300) {
+              return res.json();
+            } else {
+              return Promise.reject(new Error(`got invalid status code ${res.statusCode}`));
+            }
+          })
+          .catch(err => {
+            _throwError(err);
+          });
       } else {
         _throwError(new Error('invalid arguments: credentials required'));
       }
